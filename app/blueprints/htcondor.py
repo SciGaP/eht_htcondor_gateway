b844cab57a532879421c10f14cc46b6a9fba4a88
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2024-06-25 09:28:06 -0400

    user history

--- app/blueprints/htcondor.py
+++ app/blueprints/htcondor.py
@@ -1,9 +1,30 @@
 
+import os, sys
+from dotenv import load_dotenv
+load_dotenv()
+workspace = os.path.expanduser(os.getenv("workspace"))
+
+def inituser(username,userfolder):
+    """init user folder in workspace"""
+    os.makedirs(userfolder, exist_ok=True)
+
+def userhistory(username):
+    """check job history of a user"""
+
+    userfolder = os.path.join(workspace,username)
+    if not os.path.exists(userfolder):
+        inituser(username,userfolder)
+    # find any folder list as experiment
+    # experiment_id
+    experiments = [x for x in os.listdir(userfolder) if "experiment" in x]
+    ex_num = len(experiments)
+    return {"experiments": ex_num}
+
 def checkuser(username):
     """check user information"""
     userstatus = {}
     userstatus['username'] = username
-    userstatus['experiments'] = 10
+    history = userhistory(username)
+    userstatus['experiments'] = history['experiments']
     userstatus['running'] = 0
-
     return userstatus
