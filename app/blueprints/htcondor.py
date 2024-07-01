78aef8fb2020c5f2b8625b8bd07d6e3ffac60991
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2024-07-01 13:31:23 -0400

    create workspace folders

--- app/blueprints/htcondor.py
+++ app/blueprints/htcondor.py
@@ -1,19 +1,43 @@
 
+
 import os, sys
 from dotenv import load_dotenv
 load_dotenv()
-workspace = os.path.expanduser(os.getenv("workspace"))
 
-def inituser(username,userfolder):
-    """init user folder in workspace"""
-    os.makedirs(userfolder, exist_ok=True)
+def get_workspace():
+    """get the structure of workspace
+        workspace: eht_workspace
+        -- users
+            -- username
+                -- experiment_[id]
+        -- staging (holds the files for validation)
+        return a dict object
+    """
+    d_w = {}
+    workspace = os.path.expanduser(os.getenv("workspace"))
+    d_w['workspace'] = workspace
+    d_w['staging'] = os.path.join(workspace, "staging")
+    d_w['users'] = os.path.join(workspace,"users")
+    if not os.path.exists(d_w['staging']):
+        os.makedirs(d_w['staging'], exist_ok=True)    
+    if not os.path.exists(d_w["users"]):
+        os.makedirs(d_w["users"], exist_ok=True)
+    return d_w
+    
+
+
+def get_userfolder(username):
+    """return the folder for a user"""
+    workspace = get_workspace()
+    userfolder = os.path.join(workspace["users"], username)
+    if not os.path.exists(userfolder):
+        os.makedirs(userfolder, exist_ok=True)
+    return userfolder
 
 def userhistory(username):
     """check job history of a user"""
 
-    userfolder = os.path.join(workspace,username)
-    if not os.path.exists(userfolder):
-        inituser(username,userfolder)
+    userfolder = get_userfolder(username)
     # find any folder list as experiment
     # experiment_id
     experiments = [x for x in os.listdir(userfolder) if "experiment" in x]
