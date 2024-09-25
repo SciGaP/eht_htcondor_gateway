38691157191a7b711258da5d80befb62d1516da0
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2024-09-25 11:25:53 -0400

    submit form

--- app/blueprints/auth_logic.py
+++ app/blueprints/auth_logic.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from app import db
 
 auth_blueprint = Blueprint('auth', __name__)
@@ -7,6 +7,12 @@ auth_blueprint = Blueprint('auth', __name__)
 @auth_blueprint.route('/signup')
 def signup():
     return render_template('EHTGatewayRegister.html')
+
+@auth_blueprint.route('/signup', methods=['POST'])
+def signup_post():
+    # code to validate and add user to database goes here
+    userName = request.form.get('userName')
+    return userName
 
 @auth_blueprint.route('/login')
 def login():
