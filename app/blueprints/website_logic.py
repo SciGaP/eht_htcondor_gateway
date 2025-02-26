358d52276258e8f649fe0f96348a994df45bb184
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2025-02-25 20:33:22 -0500

    #12 still testing

--- app/blueprints/website_logic.py
+++ app/blueprints/website_logic.py
@@ -147,4 +147,4 @@ def settings():
 # @login_required
 def jupyterlab():
     # run jupyterlab
-    return redirect("http://eht.scigap.org:8888/lab", token="my-token")
+    return redirect('http://eht.scigap.org:8888/lab?token="my-token"')
